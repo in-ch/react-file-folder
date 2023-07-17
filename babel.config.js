4e7579ee12e279b46b7f35ebda344ca03b0428d3
in-ch <sxin2949@naver.com>
2023-07-17 21:19:27 +0900

    fix(babel): typeError: [BABEL]: Invalid host defined options

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: [
-    '@emotion/babel-plugin',
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     ['@babel/plugin-proposal-private-methods', { loose: true }],
     '@babel/plugin-transform-runtime',
