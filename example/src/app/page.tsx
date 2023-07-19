ec509f871598d95183955b7285ba537fefe40492
in-ch <sxin2949@naver.com>
2023-07-19 23:20:45 +0900

    chore: make example

--- example/src/app/page.tsx
+++ example/src/app/page.tsx
@@ -1,95 +1,87 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+
+import  { createFileTree, Directory, ToggleFileTree } from 'react-toggle-file-tree';
 
 export default function Home() {
+
+    const handleFileClick = () => {
+      console.log('handleFileClick');
+    }
+
+    const handleDirectoryClick = () => {
+      console.log('handleDirectoryClick');
+    }
+  const list = [
+    {
+      localPath: "/inch.txt",
+      properties: {
+        size: "1 bit",
+        anything: "possible",
+        a: "b",
+      },
+    },
+    {
+      localPath: "/fruit/apple.png",
+      properties: {
+        sizezz: "12mb",
+        azvv: "asdfasd",
+      },
+    },
+    {
+      localPath: "/fruit/banana.png",
+      properties: {
+        sizezz: "12mb",
+        azvv: "asdfasd",
+      },
+    },
+    {
+      localPath: "/vehicle/car/truck.mp4",
+      properties: {
+        sizezz: "12mb",
+        azvv: "asdfasd",
+      },
+    },
+    {
+      localPath: "/vehicle/car/taxi.svg",
+      properties: {
+        sizezz: "12mb",
+        azvv: "asdfasd",
+      },
+    },
+    {
+      localPath: "/plant/tree/oak.png",
+      properties: {
+        sizezz: "10mb",
+        azvv: "asdfasd",
+      },
+    },
+    {
+      localPath: "/plant/tree/maple.png",
+      properties: {
+        sizezz: "8mb",
+        azvv: "asdfasd",
+      },
+    },
+    {
+      localPath: "/plant/flower/cherry-blossom.png",
+      properties: {
+        sizezz: "5mb",
+        azvv: "asdfasd",
+      },
+    },
+    {
+      localPath: "/plant/fruit/apple-tree.png",
+      properties: {
+        sizezz: "15mb",
+        azvv: "asdfasd",
+      },
+    },
+  ];
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <ToggleFileTree
+      list={createFileTree(list) as Directory}
+      handleFileClick={handleFileClick}
+      handleDirectoryClick={handleDirectoryClick}
+    />
   )
 }
