58ca5375fc7835874d7752e7555571a35fcaf300
in-ch <sxin2949@naver.com>
2024-02-16 11:38:31 +0900

    build: remove unused packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,19 @@
 module.exports = {
-  extends: ['plugin:react-hooks/recommended', 'plugin:@typescript-eslint/base'],
+  extends: [],
   parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     es6: true,
     node: true,
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react'],
   rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        args: 'after-used',
-        argsIgnorePattern: '^event$',
-        ignoreRestSiblings: true,
-        vars: 'all',
-        varsIgnorePattern: 'jsx|emotionJSX',
-      },
-    ],
     curly: [2, 'multi-line'],
     'jsx-quotes': 1,
     'no-shadow': 0,
-    '@typescript-eslint/no-shadow': 2,
     'no-trailing-spaces': 1,
     'no-underscore-dangle': 1,
-    '@typescript-eslint/no-unused-expressions': 1,
     'object-curly-spacing': [1, 'always'],
-    '@typescript-eslint/quotes': [2, 'single', 'avoid-escape'],
     'react/jsx-boolean-value': 1,
     'react/jsx-no-undef': 1,
     'react/jsx-uses-react': 1,
@@ -38,8 +25,6 @@ module.exports = {
     'react/react-in-jsx-scope': 1,
     'react/self-closing-comp': 1,
     'react/sort-prop-types': 1,
-    '@typescript-eslint/semi': 2,
-    '@typescript-eslint/no-inferrable-types': 2,
     strict: 0,
   },
   settings: {
