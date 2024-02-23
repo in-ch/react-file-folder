b5e13c753c291cd31e260ae95ef22e4f655a8938
in-ch <sxin2949@naver.com>
2024-02-23 22:09:53 +0900

    feat(react-toggle-file-list): add arrow icon (#2)

---
+++ packages/react-toggle-file-tree/src/toggle-file-tree/icons/arrow-down.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { RxTriangleDown } from 'react-icons/rx';
+
+/**
+ * @description Down Arrow component
+ * @returns {JSX.Element}
+ */
+export default function ArrowDown(): JSX.Element {
+  return <RxTriangleDown className="down" />;
+}
+
+ArrowDown.displayName = 'ArrowDown';
---
+++ packages/react-toggle-file-tree/src/toggle-file-tree/icons/arrow-right.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { RxTriangleRight } from 'react-icons/rx';
+
+/**
+ * @description Right Arrow component
+ * @returns {JSX.Element}
+ */
+export default function ArrowRight(): JSX.Element {
+  return <RxTriangleRight className="right" />;
+}
+
+ArrowRight.displayName = 'ArrowRight';
